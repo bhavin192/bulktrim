fe4d9ef72baceb8df86f97a1f3f8e25a7ebf591f
Bhavin Gandhi <bhavin7392@gmail.com>
2017-07-26 08:30:12 +0530

    Port to Python3 this also fixes paths in order to work on GNU/Linux

--- convert.py
+++ convert.py
@@ -4,18 +4,18 @@ import glob
 output_choice = "n"
 
 def print_line():
-    print 90 * "=" + "\n"
+    print(90 * "=")
 
 print_line()
-print "Bulk Trim Videos Python Script by Bhavin, Using ffmpeg"
+print("Bulk Trim Videos Python Script by Bhavin, Using ffmpeg")
 print_line()
 
-list_of_files = glob.glob("input\*.mp4")
+list_of_files = glob.glob("input/*.mp4")
 #print list_of_files
 if list_of_files == []:
-    print "Input Directory is empty!!"
+    print("Input Directory is empty!")
 else :
-    output_choice = raw_input("Do you want to create output directory?, if it exist it will give error, [y/N]  ")
+    output_choice = input("Do you want to create output directory?, if it exist it will give error, [y/N]  ")
 
 if output_choice == "Y" or output_choice == "y":
     os.mkdir("output")
@@ -24,8 +24,8 @@ if output_choice == "Y" or output_choice == "y":
 ###### FFMPEG to all .mp4 files ######
 for i in list_of_files:
     print_line()
-    print "Trimming " + i
+    print("Trimming " + i)
     print_line()
-    output_name = i.lstrip("input\\")
-    os.system('ffmpeg -ss 00:00:00.0 -i "' + i + '" -c copy -t 00:22:29.963 "output\\' + output_name + '"')
+    output_name = i.lstrip("input/")
+    os.system('ffmpeg -ss 00:00:00.0 -i "' + i + '" -c copy -t 00:22:29.963 "output/' + output_name + '"')
     
